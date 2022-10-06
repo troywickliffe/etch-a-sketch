08cb6591aa752fff0c8c82d73fc78940b6c32721
troywickliffe <108986639+troywickliffe@users.noreply.github.com>
2022-10-06 14:58:47 -0400

    Add createGrid function

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-const grid = () => {
-    const newDivs = document.createElement('div');
-    const divGrid = document.getElementById('container');
-    document.body.insertBefore(newDivs, divGrid);
-}
+document.addEventListener('DOMContentLoaded', function() {
+    createGrid(32)
+    console.log('hi')
+})
 
-grid();
+function createGrid(size) {
+    let container = document.querySelector('.container');
+
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    let numDivs = size * size;
+
+    for (let i = 0; i < numDivs; i++) {
+        let div = document.createElement('div');
+        div.style.backgroundColor = 'yellow';
+        container.insertAdjacentElement('beforeend', div);
+    }
+}
