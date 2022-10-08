3689f5ddafda5c142c1b438a6bffbd40b3e7e675
troywickliffe <108986639+troywickliffe@users.noreply.github.com>
2022-10-08 16:49:47 -0400

    Add click to draw function

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 let color = 'black';
+let click = false;
 
 //Event listener so html and css load first then JavaScript DOM starts
 document.addEventListener('DOMContentLoaded', function() {
     createGrid(16);
+
+    document.querySelector('body').addEventListener('click', function(e) {
+        if(e.target.tagName != 'BUTTON') {
+            click = !click;
+            let draw = document.querySelector('#draw');
+            if(click) {
+                draw.textContent = 'You Are Allowed to Draw ';
+            }
+            else {
+                draw.textContent = 'You Are Not Allowed To Draw'
+            }
+        }
+    })
 
     let btnPop = document.querySelector('#pop')
     btnPop.addEventListener('click', function() {
@@ -45,12 +59,14 @@ function getSize() {
 }
 
 function colorDiv(){
-    if(color == 'rainbow') {
-        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
-    }
-    else {
-        this.style.backgroundColor = 'black';
-    }
+    if(click) {
+        if(color == 'rainbow') {
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+        }
+        else {
+            this.style.backgroundColor = 'black';
+        }
+    }    
 }
 
 function setColor(colorChoice) {
