b4a45b4be83dbb2268c90b590575ceeafdf3cdbf
troywickliffe <108986639+troywickliffe@users.noreply.github.com>
2022-10-08 16:37:36 -0400

    Add clearBoard function to drawing grid

--- app.js
+++ app.js
@@ -56,3 +56,8 @@ function colorDiv(){
 function setColor(colorChoice) {
     color = colorChoice;
 }
+
+function clearBoard() {
+    let divs = document.querySelectorAll('div')
+    divs.forEach((div) => div.style.backgroundColor = 'white')
+}
